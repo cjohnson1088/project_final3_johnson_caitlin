b2583733ce35d0e758a45b923872d2840bfeb084
Caitlin Johnson <caitlin.johnson3130@gmail.com>
2017-07-31 10:15:56 -0400

    add custom jQuery to change color of nav on hover

---
+++ js/main.js
@@ -0,0 +1,7 @@
+$(document).ready(function () {
+    $('.nav-tab').hover(function () {
+        $(this).css('color', '#0140b1');
+    }, function () {
+        $(this).css('color', '#412c01');
+    });
+});
